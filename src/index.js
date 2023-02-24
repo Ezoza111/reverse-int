884ebb92956e32c33218acaf292a215452fe0aa7
ezoza <ezozakhudayberdieva@mail.ru>
2023-02-24 21:37:04 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+ let nStr = String(n);
+ let splitN = nStr.split("");
+ let reverseN = splitN.reverse();
+ let joinN = reverseN.join("");
+ return +joinN;
+};
+
