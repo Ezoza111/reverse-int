3c0ac16fb3cbf70ce0500f3ad4a29a13cc525273
ezoza <ezozakhudayberdieva@mail.ru>
2023-02-24 22:01:36 +0300

    feat: create elements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 module.exports = function reverse (n) {
- let nStr = String(n);
- let splitN = nStr.split("");
- let reverseN = splitN.reverse();
- let joinN = reverseN.join("");
- return +joinN;
+    let m;
+    n < 0? m= Math.abs(n) : m = n;
+    let nStr = String(m);
+    let splitN = nStr.split("");
+    let reverseN = splitN.reverse();
+    let joinN = reverseN.join("");
+    let joinNwithout;
+    joinN[0] == "0"? joinNwithout = joinN.slice(1) : 
+    joinNwithout = joinN;
+    return joinNwithout;
 };
 
